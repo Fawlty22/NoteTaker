95017371c945174fe76eff93de9d2694522f6401
Matt Keys <MatthewjKeys22@gmail.com>
2021-11-21 18:17:10 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
